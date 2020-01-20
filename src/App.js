56ef2104129020ed7cec23415dc210464b45dc7c
Adson Wesley <adsonxzx@gmail.com>
2020-01-20 13:24:12 -0300

    configurando guia de estilo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>gym point</h1>
+  return (
+    <div>
+      <h1>adsadas asd asd asd asd asd asd asd asd asd asda sda sd</h1>
+    </div>
+  );
 }
 
 export default App;
