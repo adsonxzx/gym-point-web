2b3bd325bf29b426c32434348a0f666308f62a2e
Adson Wesley <adsonxzx@gmail.com>
2020-01-20 17:21:33 -0300

    pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
+import { Container, Form } from './styles';
+import logo from '../../assets/images/logo.svg';
+
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <Container>
+      <Form>
+        <img src={logo} alt="gym point" />
+
+        <label htmlFor="email">SEU EMAIL</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="exemplo@email.com"
+        />
+
+        <label htmlFor="password">SEU EMAIL</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="*********"
+        />
+
+        <button type="submit"> Entrar no sistema </button>
+      </Form>
+    </Container>
+  );
 }
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #ee4d64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const Form = styled.form`
+  max-width: 360px;
+  width: 100%;
+  background: #fff;
+  border-radius: 4px;
+  padding: 36px 50px;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    margin-bottom: 36px;
+  }
+
+  label {
+    color: #444444;
+  }
+
+  input {
+    height: 45px;
+    border-radius: 4px;
+    border: 1px solid #dddddd;
+    padding: 10px;
+    margin-bottom: 20px;
+    margin-top: 10px;
+    color: #444444;
+
+    &::placeholder {
+      color: #999999;
+    }
+  }
+
+  button {
+    background: #ee4d64;
+    border-radius: 4px;
+    color: #fff;
+    height: 45px;
+    border: 0;
+    font-weight: bold;
+    transition: background 0.2s;
+
+    &:hover {
+      background: #de4359;
+    }
+  }
+`;
