c7029215bab4815dc86cce1ef82c50646ceb0a1d
Adson Wesley <adsonxzx@gmail.com>
2020-02-17 17:40:28 -0300

    ajuste siderbar

--- src/componets/Sidebar/index.js
+++ src/componets/Sidebar/index.js
@@ -11,33 +11,40 @@ import { Container } from './styles';
 import logo from '../../assets/images/logo-gym.svg';
 
 export default function Sidebar() {
+  const { pathname } = window.location;
   return (
     <Container>
-      <Link to="/alunos">
+      <Link to="/alunos" className={pathname === '/alunos' ? 'active' : ''}>
         <img src={logo} alt="Gym point" />
       </Link>
 
       <ul>
         <li>
-          <Link to="/alunos">
+          <Link to="/alunos" className={pathname === '/alunos' ? 'active' : ''}>
             <MdPerson size={24} color="#CBCED6" />
             Alunos
           </Link>
         </li>
         <li>
-          <Link to="/planos" className="active">
+          <Link to="/planos" className={pathname === '/planos' ? 'active' : ''}>
             <MdConfirmationNumber size={22} color="#CBCED6" />
             Planos
           </Link>
         </li>
         <li>
-          <Link to="/matriculas">
+          <Link
+            to="/matriculas"
+            className={pathname === '/matriculas' ? 'active' : ''}
+          >
             <MdPortrait size={22} color="#CBCED6" />
             Matriculas
           </Link>
         </li>
         <li>
-          <Link to="/auxilios">
+          <Link
+            to="/auxilios"
+            className={pathname === '/auxilios' ? 'active' : ''}
+          >
             <MdAssignment size={22} color="#CBCED6" />
             Auxílio
           </Link>
